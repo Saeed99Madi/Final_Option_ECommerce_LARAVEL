75a2f512b45d44fc773f8fc7607e853aac449dde
Saeed99Madi <saed.zooro@gmail.com>
2021-08-20 21:19:00 +0300

    add_Sale_Setting_updater_admin

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\Admin\AdminEditProductComponent;
 use App\Http\Livewire\Admin\AdminHomeCategoryComponent;
 use App\Http\Livewire\Admin\AdminHomeSliderComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminSaleComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
@@ -71,5 +72,6 @@ Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function ()
     Route::get('/admin/slider/add',AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
     Route::get('/admin/slider/Edit/{slider_id}',AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
     Route::get('/admin/home-categories',AdminHomeCategoryComponent::class)->name('admin.homecategories');
+    Route::get('/admin/sale',AdminSaleComponent::class)->name('admin.sale');
 
 });
