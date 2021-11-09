fe4eb61fea38e80268158317ebee00aca7821d22
Saeed99Madi <saed.zooro@gmail.com>
2021-11-09 16:06:07 +0200

    fixd233

--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -7,6 +7,59 @@
             display: block !important;
         }
     </style>
+    <style>
+        .content {
+            padding-top: 40px;
+            padding-bottom: 40px;
+        }
+        .icon-stat {
+            display: block;
+            overflow: hidden;
+            position: relative;
+            padding: 15px;
+            margin-bottom: 1em;
+            background-color: #fff;
+            border-radius: 4px;
+            border: 1px solid #ddd;
+        }
+        .icon-stat-label {
+            display: block;
+            color: #999;
+            font-size: 13px;
+        }
+        .icon-stat-value {
+            display: block;
+            font-size: 28px;
+            font-weight: 600;
+        }
+        .icon-stat-visual {
+            position: relative;
+            top: 22px;
+            display: inline-block;
+            width: 32px;
+            height: 32px;
+            border-radius: 4px;
+            text-align: center;
+            font-size: 16px;
+            line-height: 30px;
+        }
+        .bg-primary {
+            color: #fff;
+            background: #d74b4b;
+        }
+        .bg-secondary {
+            color: #fff;
+            background: #6685a4;
+        }
+
+        .icon-stat-footer {
+            padding: 10px 0 0;
+            margin-top: 10px;
+            color: #aaa;
+            font-size: 12px;
+            border-top: 1px solid #eee;
+        }
+    </style>
     <div class="container" style="padding: 30px 0;">
         <div class="row">
             <div class="col-md-12">
